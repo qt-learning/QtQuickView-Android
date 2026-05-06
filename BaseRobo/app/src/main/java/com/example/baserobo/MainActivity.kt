c7461b0e0ade5f87d56182d509f6f4dd4985bb74
qtrampe <rami.potinkara@qt.io>
2026-05-06 14:30:47 +0300

    SafeArea margins support
    This patch adds the SafeArea margins a.k.a. edge-to-edge
    support.

--- BaseRobo/app/src/main/java/com/example/baserobo/MainActivity.kt
+++ BaseRobo/app/src/main/java/com/example/baserobo/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.baserobo
 
 import android.os.Bundle
+import android.util.TypedValue
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,9 +20,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val tv = TypedValue()
+        val actionBarHeight = if (theme.resolveAttribute(android.R.attr.actionBarSize, tv, true))
+            TypedValue.complexToDimensionPixelSize(tv.data, resources.displayMetrics) else 0
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.container) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updatePadding(top = insets.top, bottom = insets.bottom)
+            windowInsets
+        }
 
         val navView: BottomNavigationView = binding.navView
 
